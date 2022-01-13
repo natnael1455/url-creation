c87d8ad7358dc2346f8b64ec06df983a4b3ef63c
unknown <natnael1159@gmail.com>
2022-01-13 16:19:22 +0200

    add url

---
+++ script.js
@@ -0,0 +1,108 @@
+var pay_load={};
+
+// url creating function 
+function create_url(){
+	var url = "https://t.me/kamata_test_bot?start=";
+	var pay_loads="o_x"
+	var i=0;
+   for(let key in pay_load) {
+	   var short_code=key;
+	   var value = pay_load[key];
+	   if(value !=0){
+		
+	   if(i==0){
+	   pay_loads=pay_loads+"{"+short_code+"}_{"+value+"}";
+	   }
+	   else{
+		pay_loads=pay_loads+"_{"+short_code+"}_{"+value+"}";
+	   }
+	   i++;
+	}
+   }
+   if(pay_loads.length<=64){
+   alert(url+pay_loads);
+   } 
+   else{
+	alert("the size of the payload is more than 64 byte remove some items"); 
+   }
+}
+
+
+
+$('.visibility-cart').on('click',function(){
+       
+  var $btn =  $(this);
+  var $cart = $('.cart');
+  console.log($btn);
+  
+  if ($btn.hasClass('is-open')) {
+     $btn.removeClass('is-open');
+     $btn.text('O')
+     $cart.removeClass('is-open');     
+     $cart.addClass('is-closed');
+     $btn.addClass('is-closed');
+  } else {
+     $btn.addClass('is-open');
+     $btn.text('X')
+     $cart.addClass('is-open');     
+     $cart.removeClass('is-closed');
+     $btn.removeClass('is-closed');
+  }
+
+                  
+});
+
+	// SHOPPING CART PLUS OR MINUS
+	$('a.qty-minus').on('click', function(e) {
+	
+		e.preventDefault();
+		var $this = $(this);
+		var parent = $this.closest('div').attr('id');
+		var $input = $this.closest('div').find('input');
+		var value = parseInt($input.val());
+    
+		if (value > 1) {
+			value = value - 1;
+		} else {
+			value = 0;
+		}
+    
+    $input.val(value);
+        pay_load[parent] = value;
+	});
+
+	$('a.qty-plus').on('click', function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		var parent = $this.parent().attr('id');
+		var $input = $this.closest('div').find('input');
+		var value = parseInt($input.val());
+
+		if (value < 100) {
+		value = value + 1;
+		} else {
+			value =100;
+		}
+
+		$input.val(value);
+		pay_load[parent] = value;
+	});
+
+
+	$('a.btn-update').on('click', function(e) {
+		create_url();
+
+	})
+// RESTRICT INPUTS TO NUMBERS ONLY WITH A MIN OF 0 AND A MAX 100
+$('input').on('blur', function(){
+
+	var input = $(this);
+	var value = parseInt($(this).val());
+
+		if (value < 0 || isNaN(value)) {
+			input.val(0);
+		} else if
+			(value > 100) {
+			input.val(100);
+		}
+});
