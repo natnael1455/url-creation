24fba9d1a216e3c732b8277d46a1e593bf775ec6
natnael1455 <natnael1159@gmail.com>
2022-02-10 13:03:13 +0200

    feat: add click cli to the html_generator file

--- html_generator.py
+++ html_generator.py
@@ -1,3 +1,6 @@
+import os
+
+import click
 import pandas as pd
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -13,11 +16,11 @@ def product_section(index, row):
         row1 = True
     else:
         row2 = True
-    product_name = row["name"]
-    unit_price = row["unit_price"]
-    currency = row["currency"]
-    code = row["short_code"]
-    img = row["img"]
+    product_name = row["Name"]
+    unit_price = row["Unit price"]
+    currency = row["Currency"]
+    code = row["Short code"]
+    img = row["URL / image"]
 
     return template.render(
         row1=row1,
@@ -43,21 +46,45 @@ def category_section(key, category_data):
     return template.render(CategoryName=key, item=pro_section)
 
 
-env = Environment(
-    loader=PackageLoader("html_generator"), autoescape=select_autoescape()
+@click.option(
+    "--csv_file",
+    default="product.csv",
+    help="The csv file from where the html file will be generated defult is product.csv",
 )
-template = env.get_template("index.html")
+@click.option(
+    "--html_file",
+    default="advanced.html",
+    help="The name of html that will be generated defult is advanced.html",
+)
+@click.option(
+    "--categories",
+    default=False,
+    help="if True with category row and if False with out category row defult is False",
+)
+@click.command()
+def generate(csv_file, html_file, categories):
+    if os.path.isfile("./" + csv_file):
+        env = Environment(
+            loader=PackageLoader("html_generator"), autoescape=select_autoescape()
+        )
+        template = env.get_template("index.html")
+        df = pd.read_csv(csv_file)
+        grouped_df = df.groupby("category")
+        with open(html_file, "w") as htmls_file:
+            middle = ""
+            if categories:
+                for key, _item in grouped_df:
+                    category_data = grouped_df.get_group(key)
+                    middle = middle + category_section(key, category_data)
+            else:
+                for key, item in df.iterrows():
+                    middle = middle + product_section(key, item)
+            htmls_file.write(template.render(category=middle))
+            htmls_file.close()
+        click.echo("Hello World!")
+    else:
+        click.echo("the csv file " + csv_file + " doest not exist")
 
-df = pd.read_csv("product.csv")
 
-grouped_df = df.groupby("category")
-
-
-with open("advanced.html", "w") as html_file:
-    middle = ""
-    for key, _item in grouped_df:
-        category_data = grouped_df.get_group(key)
-        middle = middle + category_section(key, category_data)
-
-    html_file.write(template.render(category=middle))
-    html_file.close()
+if __name__ == "__main__":
+    generate()
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var pay_load = {};
+let pay_load = {};
 // url creating function
-function create_url() {
-    var url = 'https://t.me/kamata_test_bot?start=';
-    var pay_loads = 'o_x';
-    var i = 0;
+const create_url = () => {
+    let url = 'https://t.me/kamata_test_bot?start=';
+    let pay_loads = 'o_x';
+    let i = 0;
     for (let key in pay_load) {
-        var short_code = key.replace(/\s/g, '');
-        var value = pay_load[key];
+        let short_code = key.replace(/\s/g, '');
+        let value = pay_load[key];
         if (value != 0) {
             if (i == 0) {
                 pay_loads = pay_loads + '' + short_code + '_' + value;
@@ -26,66 +26,45 @@ function create_url() {
     else if (pay_loads === 'o_x') {
         alert('you did not select any item');
     } else {
-        var URL = url + pay_loads;
+        let URL = url + pay_loads;
         window.open(URL, '_blank');
     }
-}
-
-$('.visibility-cart').on('click', function () {
-    var $btn = $(this);
-    var $cart = $('.cart');
-    console.log($btn);
-
-    if ($btn.hasClass('is-open')) {
-        $btn.removeClass('is-open');
-        $btn.text('O');
-        $cart.removeClass('is-open');
-        $cart.addClass('is-closed');
-        $btn.addClass('is-closed');
-    } else {
-        $btn.addClass('is-open');
-        $btn.text('X');
-        $cart.addClass('is-open');
-        $cart.removeClass('is-closed');
-        $btn.removeClass('is-closed');
-    }
-});
+};
 
 // SHOPPING CART PLUS OR MINUS
-$('a.qty-minus').on('click', function (e) {
-    e.preventDefault();
-    var $this = $(this);
-    var parent = $this.closest('div').attr('id');
-    var price = parseFloat($this.parent().parent().children('#price').text());
-    var pro_total = $this.parent().parent().children('#pro-total');
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
-    var over_all = $('#over-all');
-    var over_all_total = parseFloat(over_all.text());
+$('a.qty-minus').on('click', (event) => {
+    event.preventDefault();
+    let $this = $(event.currentTarget);
+    let parent = $this.closest('div').attr('id');
+    let price = parseFloat($this.parent().parent().children('#price').text());
+    let pro_total = $this.parent().parent().children('#pro-total');
+    let $input = $this.closest('div').find('input');
+    let value = parseInt($input.val());
+    let over_all = $('#over-all');
+    let over_all_total = parseFloat(over_all.text());
     if (value > 0) {
         value = value - 1;
         over_all_total = over_all_total - price;
     } else {
         value = 0;
     }
-    var total = value * price;
+    let total = value * price;
     pro_total.children().text(total.toFixed(2));
     over_all.text(over_all_total.toFixed(2));
     $input.val(value);
     pay_load[parent] = value;
 });
 
-$('a.qty-plus').on('click', function (e) {
-    e.preventDefault();
-    var $this = $(this);
-    var parent = $this.parent().attr('id');
-    var price = parseFloat($this.parent().parent().children('#price').text());
-    var pro_total = $this.parent().parent().children('#pro-total');
-    console.log(pro_total);
-    var over_all = $('#over-all');
-    var over_all_total = parseFloat(over_all.text());
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
+$('a.qty-plus').on('click', (event) => {
+    event.preventDefault();
+    let $this = $(event.currentTarget);
+    let parent = $this.parent().attr('id');
+    let price = parseFloat($this.parent().parent().children('#price').text());
+    let pro_total = $this.parent().parent().children('#pro-total');
+    let over_all = $('#over-all');
+    let over_all_total = parseFloat(over_all.text());
+    let $input = $this.closest('div').find('input');
+    let value = parseInt($input.val());
     if (value < 100) {
         value = value + 1;
         over_all_total = over_all_total + price;
@@ -93,21 +72,22 @@ $('a.qty-plus').on('click', function (e) {
         value = 100;
     }
 
-    var total = value * price;
-    console.log(total);
+    let total = value * price;
     pro_total.children().text(total.toFixed(2));
     over_all.text(over_all_total.toFixed(2));
     $input.val(value);
     pay_load[parent] = value;
 });
 
-$('a.btn-update').on('click', function () {
+$('a.btn-update').on('click', () => {
     create_url();
 });
+
 // RESTRICT INPUTS TO NUMBERS ONLY WITH A MIN OF 0 AND A MAX 100
-$('input').on('blur', function () {
-    var input = $(this);
-    var value = parseInt($(this).val());
+$('input').on('blur', (event) => {
+    event.preventDefault();
+    let input = $(event.currentTarget);
+    let value = parseInt($(event.currentTarget).val());
 
     if (value < 0 || isNaN(value)) {
         input.val(0);
