c9a4266a17377db40d69664b4c5f8602f91ecc74
natnael1455 <natnael1159@gmail.com>
2022-05-09 15:03:13 +0300

    fead: added the publisher

--- pub-sub/publisher.py
+++ pub-sub/publisher.py
@@ -1,22 +1,52 @@
-# import csv
+import csv
 import json
+from itertools import groupby
 
+import click
 from google.cloud import pubsub_v1
 
-# TODO(developer)
-project_id = "baia-project"
-topic_id = "catalogTopic"
 
-publisher = pubsub_v1.PublisherClient()
-# The `topic_path` method creates a fully qualified identifier
-# in the form `projects/{project_id}/topics/{topic_id}`
-topic_path = publisher.topic_path(project_id, topic_id)
+def key_func(key):
+    return key["category"]
 
-js = {"name": "selam"}
-data = json.dumps(js)
-data = data.encode("utf-8")
-# When you publish a message, the client returns a future.
-future = publisher.publish(topic_path, data)
-print(future.result())
 
-print(f"Published messages to {topic_path}.")
+def csv_reader(user_name, csv_file):
+    services = {}
+    with open("../input/" + csv_file, "r") as file:
+        csv_data = csv.DictReader(file)
+        for category, service in groupby(csv_data, key_func):
+            services[category] = list(service)
+    return {"name": user_name, "items": services}
+
+
+def my_publisher(js):
+    project_id = "baia-project"
+    topic_id = "catalogTopic"
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+    data = json.dumps(js)
+    data = data.encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    click.echo(future.result())
+    click.echo(f"Published messages to {topic_path}.")
+
+
+@click.option(
+    "--csv_file",
+    default="product.csv",
+    help="""The csv file from where the html file
+    will be generated defult is product.csv""",
+)
+@click.option(
+    "--user_name",
+    required=True,
+    help="""The owner of the catalog""",
+)
+@click.command()
+def main(user_name, csv_file):
+    js = csv_reader(user_name, csv_file)
+    my_publisher(js)
+
+
+if __name__ == "__main__":
+    main()
